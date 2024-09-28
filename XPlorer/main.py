2b15ffb85306c21679f40efe375f1d8bd65b3c2c
Laavawalker <77886311+Laavawalker@users.noreply.github.com>
2024-09-28 15:46:58 +0200

    First update may not working

--- XPlorer/main.py
+++ XPlorer/main.py
@@ -4,10 +4,11 @@ import shutil
 import os
 import ctypes
 import pathlib
+import pickle
 from turtle import bgcolor
 import tkinter as tk
-import win32gui
-import win32con
+# import win32gui
+# import win32con
 
 # Increas Dots Per inch so it looks sharper
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
@@ -27,7 +28,7 @@ class CustomTitleBar(Frame):
         self.close_button.pack(side="right", padx=5)
 
         self.minimize_button = Button(self, text="-", command=self.master.iconify, bg="#333333", fg="#ffffff", bd=0)
-        self.minimize_button.pack(side="left", padx=5)
+        self.minimize_button.pack(side="right", padx=5)
 
         self.title_label = tk.Label(self, text="XPlorer", font=("Segoe UI", 12), bg="#333333", fg="#ffffff")
         self.title_label.pack(side="left", padx=5)
@@ -35,7 +36,7 @@ class CustomTitleBar(Frame):
 root = Tk()
 # set a title for our file explorer main window
 root.title('XPlorer')
-root.iconbitmap("icon.ico")
+# root.iconbitmap("./icon.ico")
 root.geometry("900x500")
 root.configure(bg="#222222")
 root.overrideredirect(True)
@@ -46,7 +47,26 @@ custom_title_bar.pack(fill="x")
 root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(1, weight=1)
 
+def drag_window(event):
+    global last_x, last_y
+    last_x, last_y = event.x, event.y
+
+def drag_window_motion(event):
+    global last_x, last_y
+    x = root.winfo_x() + event.x - last_x
+    y = root.winfo_y() + event.y - last_y
+    last_x, last_y = event.x, event.y
+    root.geometry("+%s+%s" % (x, y))
+
+custom_title_bar.bind("<Button-1>", drag_window)
+custom_title_bar.bind("<B1-Motion>", drag_window_motion)
+
 def pathChange(*event):
+    try:
+        with open("current_path.pkl", "rb") as f:
+            currentPath.set(pickle.load(f))
+    except FileNotFoundError:
+        currentPath.set("C:")
     # Get all Files and Folders from the given Directory
     directory = os.listdir(currentPath.get())
     # Clearing the list
@@ -145,7 +165,7 @@ def about():
     top.resizable(False, False)
     top.title("About XPlorer")
     top.columnconfigure(0, weight=1)
-    top.iconbitmap("icon.ico")
+    # top.iconbitmap("icon.ico")
     top.config(bg="#333333")
     Label(top, text='About', fg="#ffffff", bg= "#333333", font="Segoe-UI 24").grid()
     Label(top, text='The icon was generated in Google ImageFX!', fg="#ffffff", bg= "#333333").grid()
@@ -161,7 +181,7 @@ def new_patch():
     top.resizable(False, False)
     top.columnconfigure(0, weight=1)
     top.columnconfigure(1, weight=1)
-    top.iconbitmap("icon.ico")
+    # top.iconbitmap("icon.ico")
     top.config(bg="#333333")
     green_box = Frame(top, bg="#4cb72c", width=125, height=125)
     green_box.grid(row=1, column=0, pady=15, padx=15, sticky="NW")
@@ -175,6 +195,9 @@ def new_patch():
 
 
 
+
+
+
 top = ''
 
 # String variables
@@ -182,7 +205,7 @@ newFileName = StringVar(root, "File.dot", 'new_name')
 currentPath = StringVar(
     root,
     name='currentPath',
-    value=pathlib.Path.cwd()
+    value='C:/'
 )
 # Bind changes in this variable to the pathChange function
 currentPath.trace('w', pathChange)
@@ -194,25 +217,13 @@ def change_up_button_color(event):
 def reset_up_button_color(event=None):
     upbtn.config(fg="#ffffff", bg="#444444")
 
-upbtn = Button(root, text='Folder Up', command=goBack, bg="#333333", fg="#ffffff", border="0")
-upbtn.place(x=0, y=0, anchor=tk.NW)
 
-upbtn.bind("<Enter>", change_up_button_color)
-
-upbtn.bind("<Leave>", reset_up_button_color)
-upbtn.bind("<ButtonRelease-1>", reset_up_button_color)
 
 
 # Keyboard shortcut for going up
 root.bind("<Alt-Up>", goBack)
-commandbar = Entry(root, textvariable=currentPath, bg="#444444", fg="#ffffff", border=0, insertbackground="#ffffff", selectbackground="#4cb72c")
-commandbar.pack(fill='x', padx=10, pady=10)
-# List of files and folder
-list = Listbox(root, bg="#222222", fg="#ffffff", selectbackground="#444444", bd=0, highlightthickness=0, highlightbackground="#333333")
-list.pack(fill='x', padx=10, pady=10)
-# List Accelerators
-list.bind('<Double-1>', changePathByClick)
-list.bind('<Return>', changePathByClick)
+
+
 
 
 class CustomMenuBar(Frame):
@@ -250,11 +261,89 @@ class CustomMenuBar(Frame):
         # Create a popup menu for Help
         menu = Menu(self.master, tearoff=0)
         menu.add_command(label="About", command=about)
+        menu.add_command(label="What's new?", command=new_patch)
         menu.tk_popup(self.help_button.winfo_x(), self.help_button.winfo_y() + self.help_button.winfo_height())
 
+
+def custom_command(event=None):
+    command = commandbar.get()
+    if command.startswith("cd "):
+        new_path = command[3:]
+        if os.path.exists(new_path):
+            currentPath.set(new_path)
+        else:
+            messagebox.showerror("Error", "Path not found")
+            print("custom_command: Error Path not found")
+    elif command == "mkdir":
+        open_popup()
+    elif command == "newpatch":
+        new_patch()
+    elif command == "about":
+        about()
+    elif command == "exit":
+        root.destroy()
+    elif command == "home":
+        change_listbox_to_homescreen()
+    else:
+        messagebox.showerror("Error", "Invalid command")
+        print("custom_command: Error Unknown command")
+    command.delete(0, END)
+    pass
 cmb = CustomMenuBar(root)
 cmb.pack(fill="x")
 cmb.config(bg="#333333")
+
+top_frame = Frame(root, bg="#222222")
+top_frame.pack(fill="x")
+
+upbtn = Button(top_frame, text='Folder Up', command=goBack, bg="#333333", fg="#ffffff", border="0")
+upbtn.pack(side=LEFT, padx=5)
+
+commandbar = Entry(top_frame, textvariable=currentPath, bg="#444444", fg="#ffffff", border=0, insertbackground="#ffffff", selectbackground="#4cb72c")
+commandbar.pack(side=LEFT, fill='x', padx=5, pady=7, expand=True)
+commandbar.bind("<Return>", custom_command)
+
+upbtn.bind("<Enter>", change_up_button_color)
+
+upbtn.bind("<Leave>", reset_up_button_color)
+upbtn.bind("<ButtonRelease-1>", reset_up_button_color)
+# List of files and folder
+list = Listbox(root, bg="#222222", fg="#ffffff", selectbackground="#444444", bd=0, highlightthickness=0, highlightbackground="#333333")
+list.pack(fill='x', padx=10, pady=10)
+# List Accelerators
+list.bind('<Double-1>', changePathByClick)
+list.bind('<Return>', changePathByClick)
+
+text_widget = Text(root, width=50, height=10)
+text_widget.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+def change_listbox_to_homescreen(event=None):
+        with open("current_path.pkl", "wb") as f:
+            pickle.dump(currentPath.get(), f)
+        global back_button
+        list.delete(0, END)
+        list.pack_forget()
+        text_widget.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        text_widget.delete(1.0, END)
+        text_widget.insert(END, "Welcome back to XPlorer!", "large_font")
+        text_widget.insert(END, "\nWhat's up, what would you do today?", "small_font")
+
+        text_widget.tag_config("large_font", font=("Segoe UI", 24), justify=CENTER)
+        text_widget.tag_config("small_font", font=("Segoe UI", 12), justify=CENTER)
+
+        text_widget.config(state=DISABLED)
+
+        back_button = Button(root, text="Back", command=show_listbox)
+        back_button.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+def show_listbox():
+    global back_button
+    text_widget.place_forget()
+    back_button.place_forget()
+    list.pack(fill=BOTH, expand=True)
+    pathChange()
+
 # Call the function so the list displays
 pathChange('')
 # run the main program
